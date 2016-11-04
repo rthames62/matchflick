ab8f6cb047a5089a82da3aaea818b9bb385cbe98
Ryan Thames <rthames62@gmail.com>
2016-11-04 15:11:44 -0500

    hi

--- public/js/controllers/loadingCtrl.js
+++ public/js/controllers/loadingCtrl.js
@@ -2,6 +2,16 @@ function loadingCtrl($scope, mainService){
   $scope.loading = function(){
     return mainService.loading();
   }
+  $scope.test = "hi";
+  $scope.getFbCurrentUser = function(){
+    console.log("from loadingctrl");
+      mainService.getFbCurrentUser().then(function(response){
+        $scope.currentUser = response;
+        return $scope.currentUser;
+      })
+  }
+
+  $scope.getFbCurrentUser();
   $scope.loading();
 }
 
--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,16 +1,16 @@
 function mainCtrl($scope, $location, mainService){
   console.log("hello from the top");
   $scope.initCounterShow = true;
-  $scope.test = "hi";
-  $scope.getFbCurrentUser = function(){
-    console.log("from mainctrl");
-      mainService.getFbCurrentUser().then(function(response){
-        $scope.currentUser = response;
-        return $scope.currentUser;
-      })
-  }
-
-  $scope.getFbCurrentUser();
+  // $scope.test = "hi";
+  // $scope.getFbCurrentUser = function(){
+  //   console.log("from mainctrl");
+  //     mainService.getFbCurrentUser().then(function(response){
+  //       $scope.currentUser = response;
+  //       return $scope.currentUser;
+  //     })
+  // }
+  //
+  // $scope.getFbCurrentUser();
 }
 
 module.exports = mainCtrl;
