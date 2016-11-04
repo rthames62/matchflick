8e2c6c7f8ecae2cddeae56b5f988980a2ce7d859
Ryan Thames <rthames62@gmail.com>
2016-11-04 16:17:47 -0500

    hi

--- public/js/controllers/loadingCtrl.js
+++ public/js/controllers/loadingCtrl.js
@@ -2,19 +2,17 @@ function loadingCtrl($scope, mainService){
   // $scope.loading = function(){
   //   return mainService.loading();
   // }
-  $scope.userLoaded = false;
+  $scope.userLoaded = mainService.userLoaded;
 
   $scope.test = "hi";
   $scope.getFbCurrentUser = function(){
       mainService.getFbCurrentUser().then(function(response){
         $scope.currentUser = response;
-        $scope.userLoaded = true;
         return $scope.currentUser;
       })
   }
 
   $scope.$watch('userLoaded', function(newValue, oldValue){
-    console.log("hello from watch");
     console.log(newValue, oldValue);
     if($scope.userLoaded){
       console.log("time to roll, TINY RICK");
